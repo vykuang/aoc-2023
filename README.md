26dfcb69a0b1ddf74071ab26ffb6d0783d552477
victor <vykuang92@gmail.com>
2023-12-03 01:58:48 +0800

    use re.search for d1 p2

---
+++ README.md
@@ -0,0 +1,26 @@
+# Advent of code 2023
+
+## day 1 p1
+
+check if each char is numeric from left to right; break once we find it.
+
+To find the last one, reverse the char order in the line
+
+## day 1 p2
+
+Wholesale replacement is invalidated by `eightwothree`; the order in which we replace the string becomes critical, and wholesale replacement cannot account for that.
+
+I considered some arrangement that took into account possible overlaps, e.g. `eight` must be replaced before `two`, but `one` makes the ordering irreconcilable. If `eight` is before `two`, then `one` must be before `eight`, however `two` must be replaced before `one`.
+
+Try `re.search`; note that `re.match` only looks at *beginning of string*
+
+```py
+import re
+
+inp = 'eightwothree'
+p = re.compile('two')
+r = p.match(inp)
+r.span()[0] # 4
+```
+
+Obtaining the `start` allows us to replace the *first* and *last* occurring string digit
--- aoc_2023/d1/day1.py
+++ aoc_2023/d1/day1.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from pathlib import Path
 import argparse
 
@@ -10,53 +11,49 @@ def read_line(fpath: str):
 
 
 def main(use_sample: bool, part_two: bool):
-    if use_sample:
-        fp = "sample.txt"
+    if use_sample and not part_two:
+        fp = "sample1.txt"
+    elif use_sample and part_two:
+        fp = "sample2.txt"
     else:
         fp = "input.txt"
+
+    print(f"using file {fp}")
     total = 0
-    # part 1
+    # part 2
+    digits_str = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+    ]
+    digits_dict = {s: str(i + 1) for i, s in enumerate(digits_str)}
     for line in read_line(fp):
-        if not part_two:
-            for ch in line:
-                if ch.isnumeric():
-                    digit1 = int(ch)
-                    break
-            for ch in line[::-1]:
-                if ch.isnumeric():
-                    digit2 = int(ch)
-                    break
-            cal = digit1 * 10 + digit2
-            if use_sample:
-                print(cal)
-            total += cal
-        else:
-            # look for each letter
-            # if a letter does not match the first of any of these,
-            #   skip the next 2
-            # at the same time, look for ints; if it appears in the first 3 char,
-            # automatically take that
-            digits_str = [
-                "one",
-                "two",
-                "three",
-                "four",
-                "five",
-                "six",
-                "seven",
-                "eight",
-                "nine",
-            ]
-            cand1 = cand2 = None
-            for i, ch in enumerate(line):
-                if ch.isnumeric() and not cand1:
-                    if i < 3:
-                        digit1 = int(ch)
-                    else:
-                        cand1 = int(ch)
-                # if [ch == digit_str[0] for digit_str in digits_str].any():
-                # match ch:
-                #     case ['o']
+        print(f"original line: {line}")
+        if part_two:
+            # determine the first and last occurring string digit
+
+            for d in digits_dict:
+                line = line.replace(d, digits_dict[d])
+            print(f"edited line: {line}")
+
+        for ch in line:
+            if ch.isnumeric():
+                digit1 = int(ch)
+                break
+        for ch in line[::-1]:
+            if ch.isnumeric():
+                digit2 = int(ch)
+                break
+        cal = digit1 * 10 + digit2
+        if use_sample:
+            print(cal)
+        total += cal
 
     print(f"total: {total}")
 
@@ -65,6 +62,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     opt = parser.add_argument
     opt("--use_sample", "-s", action="store_true", default=False)
-    opt("--part_two", action="store_true")
+    opt("--part_two", action="store_true", default=False)
     args = parser.parse_args()
     main(args.use_sample, args.part_two)
