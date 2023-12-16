1f5a2f6a878cdfbec1a35294abdcd3f71057f887
klang <bukit.klang.83@gmail.com>
2023-12-16 15:42:29 +0800

    start day 8

--- aoc_2023/d8/day8.py
+++ aoc_2023/d8/day8.py
@@ -15,6 +15,17 @@ def read_line(fpath: str):
         yield from f
 
 
+def parse_map_entry(line: str) -> dict:
+    """
+    parse "AAA = (BBB, CCC)" to dict:
+    {'AAA': {'L': 'BBB', 'R': 'CCC'}}
+    """
+    k = line[:3]
+    l = line[7:10]
+    r = line[12:15]
+    return k, l, r
+
+
 def main(sample: bool, part_two: bool, loglevel: str):
     """ """
     logger.setLevel(loglevel)
@@ -24,6 +35,19 @@ def main(sample: bool, part_two: bool, loglevel: str):
         fp = "sample.txt"
     logger.debug(f"loglevel: {loglevel}")
     logger.info(f'Using {fp} for {"part 2" if part_two else "part 1"}')
+
+    dirs = read_line(fp)
+    logger.debug(f"dirs: {dirs}")
+    next(read_line(fp))  # skip empty line
+    node_map = {parse_map_entry(line) for line in read_line(fp)}
+    logger.debug(f"node map: {node_map}")
+    # for line in read_line(fp):
+    #    logger.debug(f'{line}')
+    #    tokens = line.split()
+    #    if len(tokens) == 1:
+    #        dirs = tokens
+    #    elif len(tokens) > 1:
+    #        node_map.update(parse_map_entry(line))
 
 
 if __name__ == "__main__":
