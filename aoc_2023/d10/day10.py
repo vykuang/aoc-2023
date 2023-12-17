5a947b324ece438b00e43a738a3101fcf94555e4
klang <bukit.klang.83@gmail.com>
2023-12-17 16:13:47 +0800

    start day10

--- aoc_2023/d10/day10.py
+++ aoc_2023/d10/day10.py
@@ -16,7 +16,15 @@ def read_line(fpath: str):
 
 
 def main(sample: bool, part_two: bool, loglevel: str):
-    """ """
+    """
+    Pathfinding?
+    Given a map of pipes, find the furthest distance from the start
+    F, 7, J, L are elbow pipes
+    -: straights
+    .: ground
+    S: start
+    """
+
     logger.setLevel(loglevel)
     if not sample:
         fp = "input.txt"
@@ -24,6 +32,17 @@ def main(sample: bool, part_two: bool, loglevel: str):
         fp = "sample.txt"
     logger.debug(f"loglevel: {loglevel}")
     logger.info(f'Using {fp} for {"part 2" if part_two else "part 1"}')
+
+    pipe_map = read_line(fp)
+    # find 'S' pos
+    # pipes could only connect n/e/s/w, so check the 4 adj squares
+    # -: - on east/west; F, L on west only, 7, J on east only
+    # |: | on north/south; J, L on south only, F, 7 on north only
+    # F: east/south; -, 7, J on east, J, L on south
+    # 7: south/west; -, F, L west, J, L south
+    # J: north/west; -, F, L west, 7, F north
+    # L: north/east; -, 7, J on east, 7, F north
+    # S: - e/w, | n/s,
 
 
 if __name__ == "__main__":
